71dd27a2570f87fdc917656a89aff5de1548e2dd
Witz0 <Witz0@users.noreply.github.com>
2016-09-30 12:52:57 -0500

    Add files via upload

--- OSE_CEB_Press_v16.09_v8.ino
+++ OSE_CEB_Press_v16.09_v8.ino
@@ -17,26 +17,27 @@ Hmm thought this was best copyleft, but previous code was CC-BY-SA need to doubl
 
 */
 
-//includes make sure correct Teensy libs get included if any are needed
+//includes 		*need to make sure correct Teensy libs get included if any are needed
 //Teensyduino add-on software v1.30 is compatible with arduino IDE 1.6.11
 //#include "Aurduino.h"
 
 //Heavy use of defines may make it easier for non coders to make adjustments for troubleshooting and custom changes
 
-#define MODE_SELECT 9
-#define SOLENOID_RIGHT 23
-#define SOLENOID_LEFT 24  //PWM capable pins
-#define SOLENOID_DOWN 25  //PWM
-#define SOLENOID_UP 26    //PWM
+#define MODE_SELECT 9	
+#define SOLENOID_RIGHT 23	
+#define SOLENOID_LEFT 24  
+#define SOLENOID_DOWN 25  
+#define SOLENOID_UP 26    
 #define PRESSURE_SENSOR 38  //A0 F0 10 bit A2D converter
-
+#define SWITCH_DEBOUNCE 3	//milliseconds to delay for switch debounce
+#define PRESSURE_SENSOR_DEBOUNCE 20	//milliseconds to delay for pressure sensor debounce
 #define COMPRESS_DELAY 500  // 1/2 sec extra to compress brick via main Cyl
-//#define DRIFT_ADJUST ?
-//#define CYLA ?
-//#define RODA ?
+#define K_A 0.001	// T_e = T_c * (k_A)		*need to find correct value assuming this methods make sense*
+
 
 // custom structures functions declarations and prototypes
 bool readMode();
+bool readPressure();
 
 void setup() {
   
@@ -67,19 +68,27 @@ Main Cylinder is set to apropriate position for block thichness
 Secondary is correctly centered in middle position
 
 */
-
 unsigned long previousMillis = 0;
+static unsigned long mainRetTime;
+static unsigned long drawerRetTime;
+static unsigned long mainExtTime;
+static unsigned long drawerExtTime;
+static float areaFraction = K_A;
 
 //poll selector switch continuously
-while(!readMode() == HIGH) {
+while(readMode()==LOW) {
 
-	//Step 1 Retraction measure T_ret until Presure sensor high
-
-	digitalWrite(SOLENOID_LEFT, HIGH);
+	//Step 1 Retraction measure T_ret at Presure sensor high
+	while(readPressure()==LOW){
+		previousMillis = millis();
+		digitalWrite(SOLENOID_LEFT, HIGH);
+	}
+	drawerRetTime = millis() - previousMillis;
 
 	//Step 2 Ejection by extending main cyl until pressure sensor high measure T_ext
-
-	//Step 3 Brick Removal 2ndCyl extended until PPresure sensor high
+	
+	
+	//Step 3 Brick Removal 2ndCyl extended until Presure sensor high
 
 	//Step 4 Soil Load main Cyl moves down and soil enters chamber measure T_ret
 
@@ -94,6 +103,29 @@ while(!readMode() == HIGH) {
 //custom functions
 
 bool readMode(){
+	if(digitalRead(MODE_SELECT)==HIGH){
+		delay(SWITCH_DEBOUNCE);
+		if(digitalRead(MODE_SELECT)==HIGH){
+			return true;
+			}
+	}
 	
-	
+	else {
+		return false;
+	}
 }
+
+bool readPressure(){
+	if(digitalRead(PRESSURE_SENSOR)==HIGH){
+		delay(PRESSURE_SENSOR_DEBOUNCE);
+		if(digitalRead(PRESSURE_SENSOR)==HIGH){
+			return true;
+			}
+	}
+	
+	else {
+		return false;
+	}
+}
+
+
